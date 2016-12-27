16f2457c56e292a6a1b0209b8a3cb67b8516b327
jshnaidman <jshnaidman245@gmail.com>
2016-12-27 14:32:48 -0500

    added more code, doesn't sort yet. Still bugged

--- 21-30/Question 22.c
+++ 21-30/Question 22.c
@@ -3,6 +3,7 @@
 
 // chose to do this low level without much help from libraries
 
+//use some assembly to get a random number
 unsigned long long rdtsc()
 {
   unsigned int lo, hi;
@@ -41,13 +42,14 @@ int compare(char *first, char *second)
   return 0;
 }
 
-void swap(char *p1, char *p2)
+void swap(void *p1, void *p2)
 {
-  char temp = *p1;
-  *p1 = *p2;
-  *p2 = temp;
+  void* temp = p1;
+  p1 = p2;
+  p2 = temp;
 }
 
+//not used
 //swaps a random pointer to the end of the array based on the median of three randomly chosen indices
 void pivot(char *array, int maxIndex)
 {
@@ -88,16 +90,61 @@ void pivot(char *array, int maxIndex)
   }
 }
 
-void quicksort(char *low, char *high)
-{
+//split everything from low to high into two lists.
+//half less than pivot, half greater than pivot
+char** partition(char** low, char** high){
+
+  if(low == high){
+    return low;
+  }
+
+  //choose a random element for a pivot
+  int randomNum = (size_t) rand();
+  char** pivot = low + randomNum%(high-low);
+  //swap their values and change pointers
+  swap(pivot,high--); 
+  //go from low to high, swapping if index i < pivot
+  char** i = low;
+  while( i != high){
+    if(compare(*i,*pivot) <= 0){
+      swap(low++,i); //once swapped, increment low to point to a new index
+    }
+    i++;
+  }
+  // at the very end, everything to the left of pivot is less than it
+  // and everything to the right of it should be greater than it
+  // therefore, need to swap low and pivot
+  swap(pivot,low); 
+  //return pointer to pivot
+  return low;
 }
+
+void quicksort(char** low, char** high)
+{
+  //base case
+  if(high<low){
+    return;
+  }
+
+  char** pivot = partition(low,high); //paritition the array into two lists, left and right of the pivot
+  quicksort(low,pivot-1); //sort the left list
+  quicksort(pivot+1, high); //sort the right list
+
+}
+
+long getBasicScore(char* name){
+    long score = 0;
+    int i;
+    while( *(name+i) != '\0'){
+      score+= *(name+i++) -64;
+    }
+    return score;
+  }
 
 int main()
 {
   srand(rdtsc());
-  char const *filePath = "/Users/jshnaidman/Documents/OneDrive - McGill "
-                         "University/Programming/Project Euler/21-30/Question "
-                         "22/q22.txt";
+  char const *filePath = "q22.txt";
   FILE *textStream = fopen(filePath, "r");
   char **nameArray =
       (char **)malloc(5163 * sizeof(char *)); // not implemented as an array
@@ -107,7 +154,7 @@ int main()
   int quoteCount;
   while ((c = getc(textStream)) != EOF)
   {
-    if (c != ',')
+    if (c > 64 && c <91)
     {
       *(*(nameArray + nameIndex) + charIndex++) = c;
     }
@@ -116,12 +163,19 @@ int main()
       quoteCount++;
       if (quoteCount % 2 == 0)
       {
-        printf("%d\n", nameIndex++);
         charIndex = 0;
-        *(nameArray + nameIndex) = malloc(40 * sizeof(char));
+        *(nameArray + ++nameIndex) = malloc(40 * sizeof(char));
       }
     }
   }
 
-  quicksort(*nameArray, *(nameArray + nameIndex));
+  quicksort(nameArray, (nameArray + nameIndex-1));
+
+  long nameScoreTotal;
+  for(int i =0; i< nameIndex;i++){
+    nameScoreTotal += getBasicScore( *(nameArray + i) );
+  }
+
+  printf("\n%ld\n", nameScoreTotal);
+
 }
