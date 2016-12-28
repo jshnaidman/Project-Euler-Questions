70b8e100526a335107b7ccac4f4838efe2f63a6d
jshnaidman <jshnaidman245@gmail.com>
2016-12-28 00:00:54 -0500

    almost done Q22, a bit of a bug with quicksort atm

--- 21-30/Question 22.c
+++ 21-30/Question 22.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// chose to do this low level without much help from libraries
+// chose to do this low level without much help from libraries - edit: had to add string library for memset
 
 //use some assembly to get a random number
 unsigned long long rdtsc()
@@ -12,149 +12,156 @@ unsigned long long rdtsc()
   return ((unsigned long long)hi << 32) | lo;
 }
 
+void *memset(void *arr, int c, size_t n)
+{
+char* str = (char*) arr;
+  for (size_t i = 0; i < n; i++)
+  {
+    *(str + i) = c;
+  }
+  return str;
+}
+
 // Compared strings alphabetically
 // returns  1 if first > second
 // returns -1 if first < second
 // returns  0 if first == second
 int compare(char *first, char *second)
 {
-  int maxIndex;
-  if (sizeof(first) > sizeof(second))
+  int compareIndex = 0;
+ while( *(first+compareIndex) != '\0' && *(second+compareIndex) != '\0')
   {
-    maxIndex = sizeof(first);
-  }
-  else
-  {
-    maxIndex = sizeof(second);
-  }
-
-  for (int i = 0; i < maxIndex; i++)
-  {
-    if (*(first + i) < *(second + i))
+    if (*(first + compareIndex) > *(second + compareIndex))
     {
       return 1;
     }
-    else if (*(first + i) > *(second + i))
+    else if (*(first + compareIndex) < *(second + compareIndex))
     {
       return -1;
     }
+    compareIndex++;
   }
   return 0;
 }
-
-void swap(void *p1, void *p2)
+//swaps values pointed at by p1 and p2, used for swapping strings
+void swap(char **p1, char**p2)
 {
-  void* temp = p1;
-  p1 = p2;
-  p2 = temp;
+  char *temp = *p1;
+  *p1 = *p2;
+  *p2 = temp;
 }
 
 //not used
 //swaps a random pointer to the end of the array based on the median of three randomly chosen indices
-void pivot(char *array, int maxIndex)
-{
-  //indices 1,2,3 respectively
-  int indexOne = rand() % (maxIndex + 1);
-  int indexTwo = rand() % (maxIndex + 1);
-  int indexThree = rand() % (maxIndex + 1);
-  if (compare((array + indexOne), (array + indexTwo)) > 0) //case 1>2
-  {
-    if (compare((array + indexThree), (array + indexOne)) > 0)
-    {
-      swap((array + maxIndex), (array + indexOne)); // case  3>1>2
-    }
-    else if (compare((array + indexOne), (array + indexThree)) > 0)
-    {
-      swap((array + maxIndex), (array + indexTwo)); // case  1>2>3
-    }
-    else
-    {
-      swap((array + maxIndex), (array + indexThree)); //case 1>3>2
-    }
-  }
+// void pivot(char *array, int maxIndex)
+// {
+//   //indices 1,2,3 respectively
+//   int indexOne = rand() % (maxIndex + 1);
+//   int indexTwo = rand() % (maxIndex + 1);
+//   int indexThree = rand() % (maxIndex + 1);
+//   if (compare((array + indexOne), (array + indexTwo)) > 0) //case 1>2
+//   {
+//     if (compare((array + indexThree), (array + indexOne)) > 0)
+//     {
+//       swap((array + maxIndex), (array + indexOne)); // case  3>1>2
+//     }
+//     else if (compare((array + indexOne), (array + indexThree)) > 0)
+//     {
+//       swap((array + maxIndex), (array + indexTwo)); // case  1>2>3
+//     }
+//     else
+//     {
+//       swap((array + maxIndex), (array + indexThree)); //case 1>3>2
+//     }
+//   }
 
-  else // case: 2>1
-  {
-    if (compare((array + indexOne), (array + indexThree)) > 0)
-    {
-      swap((array + maxIndex), (array + indexOne)); // case 2>1>3
-    }
-    else if (compare((array + maxIndex), (array + maxIndex / 2)) < 0)
-    {
-      swap((array + maxIndex), (array + indexTwo)); // case 3>2>1
-    }
-    else
-    {
-      swap((array + maxIndex), (array + indexThree)); //case 2>3>1
-    }
-  }
-}
+//   else // case: 2>1
+//   {
+//     if (compare((array + indexOne), (array + indexThree)) > 0)
+//     {
+//       swap((array + maxIndex), (array + indexOne)); // case 2>1>3
+//     }
+//     else if (compare((array + maxIndex), (array + maxIndex / 2)) < 0)
+//     {
+//       swap((array + maxIndex), (array + indexTwo)); // case 3>2>1
+//     }
+//     else
+//     {
+//       swap((array + maxIndex), (array + indexThree)); //case 2>3>1
+//     }
+//   }
+// }
 
 //split everything from low to high into two lists.
 //half less than pivot, half greater than pivot
-char** partition(char** low, char** high){
-
-  if(low == high){
-    return low;
-  }
-
+char **partition(char **low, char **high)
+{
   //choose a random element for a pivot
-  int randomNum = (size_t) rand();
-  char** pivot = low + randomNum%(high-low);
-  //swap their values and change pointers
-  swap(pivot,high--); 
-  //go from low to high, swapping if index i < pivot
-  char** i = low;
-  while( i != high){
-    if(compare(*i,*pivot) <= 0){
-      swap(low++,i); //once swapped, increment low to point to a new index
+  int randomNum = (size_t)rand();
+  char **pivot = low + randomNum % (high - low);
+  //put the pivot at the end of the list
+  swap(pivot, high); //swap values
+  char** temp = high; //swap pointers
+  high = pivot;
+  pivot = temp;
+  //go from low to high, swapping if index partitionIndex < pivot
+  char **partitionIndex = low;
+  while (partitionIndex != pivot)
+  {
+    if (compare(*partitionIndex, *pivot) < 0)
+    {
+      swap(low++, partitionIndex); //once swapped, increment low because it's been swapped
     }
-    i++;
-  }
+    partitionIndex++;
+  } 
   // at the very end, everything to the left of pivot is less than it
   // and everything to the right of it should be greater than it
-  // therefore, need to swap low and pivot
-  swap(pivot,low); 
+  // therefore, need to swap pivot with the index after low, since it comes after low
+  swap(pivot, low);
   //return pointer to pivot
   return low;
 }
 
-void quicksort(char** low, char** high)
+void quicksort(char **low, char **high)
 {
   //base case
-  if(high<low){
+  if (high <= low)
+  {
     return;
   }
 
-  char** pivot = partition(low,high); //paritition the array into two lists, left and right of the pivot
-  quicksort(low,pivot-1); //sort the left list
-  quicksort(pivot+1, high); //sort the right list
-
+  char **pivot = partition(low, high); //paritition the array into two lists, less than and greater than pivot
+  quicksort(low, pivot-1);           //sort the left list
+  quicksort(pivot + 1, high);          //sort the right list
 }
 
-long getBasicScore(char* name){
-    long score = 0;
-    int i;
-    while( *(name+i) != '\0'){
-      score+= *(name+i++) -64;
-    }
-    return score;
+long getBasicScore(char *name)
+{
+  long score = 0;
+  long charIndex = 0;
+  while (*(name + charIndex) != '\0')
+  {
+    score += *(name + charIndex++) - 64;
   }
+  return score;
+}
 
 int main()
 {
   srand(rdtsc());
   char const *filePath = "q22.txt";
   FILE *textStream = fopen(filePath, "r");
-  char **nameArray =
-      (char **)malloc(5163 * sizeof(char *)); // not implemented as an array
-  *(nameArray) = malloc(16 * sizeof(char));
+  char **nameArray = (char **)malloc(6000 * sizeof(char *)); // not implemented as an array
+  memset(nameArray, 0, 6000 * sizeof(char *));
+  *(nameArray) = malloc(40 * sizeof(char));
+  memset(*nameArray, 0, 40 * sizeof(char));
   char c;
-  int charIndex, nameIndex;
-  int quoteCount;
+  int charIndex = 0;
+  int nameIndex = 0;
+  int quoteCount = 0;
   while ((c = getc(textStream)) != EOF)
   {
-    if (c > 64 && c <91)
+    if (c > 64 && c < 91)
     {
       *(*(nameArray + nameIndex) + charIndex++) = c;
     }
@@ -165,17 +172,21 @@ int main()
       {
         charIndex = 0;
         *(nameArray + ++nameIndex) = malloc(40 * sizeof(char));
+        memset(*(nameArray + nameIndex), 0, 40 * sizeof(char));
       }
     }
   }
 
   quicksort(nameArray, (nameArray + nameIndex-1));
 
-  long nameScoreTotal;
-  for(int i =0; i< nameIndex;i++){
-    nameScoreTotal += getBasicScore( *(nameArray + i) );
+  long nameScoreTotal = 0;
+  long score;
+  for (int i = 0; i < nameIndex; i++)
+  {
+    score = getBasicScore(*(nameArray + i));
+    nameScoreTotal += (i+1)*score;
+    printf("%s\n", *(nameArray+i));
   }
 
   printf("\n%ld\n", nameScoreTotal);
-
 }
