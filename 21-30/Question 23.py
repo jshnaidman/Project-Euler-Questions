c20ba4f97358cd7a0250c7712f1844069378c73b
jshnaidman <jshnaidman245@gmail.com>
2016-12-29 21:21:43 -0500

    Finished Q23

--- 21-30/Question 23.py
+++ 21-30/Question 23.py
@@ -1,39 +1,57 @@
 import math
 
-factors = {1: [1]}
-
+abundants = []
+maxNum = 28123
 
 def factor(n):
+    i = 2
     n_factors = []
-    if(n in factors):
-        return factors.get(n)
-    i = 1
-    while i < math.sqrt(n):
+    while i <= math.sqrt(n):
         if n % i == 0:
-            if i in factors:
-                for num in factors.get(i):
-                    if num not in n_factors:
-                        n_factors.append(num)
-            else:
-                n_factors.append(i)
-                n_factors.append(n / i)
+            n_factors.append(i)
+            c = n/i
+            if c != i:
+                n_factors.append(c)
         i += 1
-    factors[n] = n_factors
     return n_factors
 
 
 def isAbundant(num):
     numFactors = factor(num)
-    total = 0
+    total = 1
     for fac in numFactors:
         total += fac
     if total > num:
         return True
     return False
 
-total = 0
-for n in range(28123):
-    if not isAbundant(n):
+def binarySearch(target,num_list):
+    low = 0
+    high = len(num_list) -1
+    while (low <= high):
+        mid = low + (high-low)/2
+        if (num_list[mid] == target):
+            return True
+        elif target < num_list[mid]:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return False
+
+
+n=0
+total=0
+while n <= maxNum:
+    found = False
+    for num in abundants:
+        if binarySearch(n-num,abundants):
+            found = True
+            break
+    if not found:
         total += n
 
-print(total)
+    if isAbundant(n):
+        abundants.append(n)
+    n += 1
+
+print total
